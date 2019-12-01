0a3457305fbd15fd8e46a1654e1b6f9e8deaa562
dollypizzle <dolapo.mabawonku@gmail.com>
2019-12-01 14:27:09 +0100

    updated ingress-service and fib.js

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -1,58 +1,58 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from 'react'
+import axios from 'axios'
 
 class Fib extends Component {
   state = {
     seenIndexes: [],
     values: {},
     index: ''
-  };
-
-  componentDidMount() {
-    this.fetchValues();
-    this.fetchIndexes();
   }
 
-  async fetchValues() {
-    const values = await axios.get('/api/values/current');
-    this.setState({ values: values.data });
+  componentDidMount () {
+    this.fetchValues()
+    this.fetchIndexes()
   }
 
-  async fetchIndexes() {
-    const seenIndexes = await axios.get('/api/values/all');
+  async fetchValues () {
+    const values = await axios.get('/api/values/current')
+    this.setState({ values: values.data })
+  }
+
+  async fetchIndexes () {
+    const seenIndexes = await axios.get('/api/values/all')
     this.setState({
       seenIndexes: seenIndexes.data
-    });
+    })
   }
 
   handleSubmit = async event => {
-    event.preventDefault();
+    event.preventDefault()
 
     await axios.post('/api/values', {
       index: this.state.index
-    });
-    this.setState({ index: '' });
-  };
-
-  renderSeenIndexes() {
-    return this.state.seenIndexes.map(({ number }) => number).join(', ');
+    })
+    this.setState({ index: '' })
   }
 
-  renderValues() {
-    const entries = [];
+  renderSeenIndexes () {
+    return this.state.seenIndexes.map(({ number }) => number).join(', ')
+  }
+
+  renderValues () {
+    const entries = []
 
     for (let key in this.state.values) {
       entries.push(
         <div key={key}>
           For index {key} I calculated {this.state.values[key]}
         </div>
-      );
+      )
     }
 
-    return entries;
+    return entries
   }
 
-  render() {
+  render () {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
@@ -70,8 +70,8 @@ class Fib extends Component {
         <h3>Calculated Values:</h3>
         {this.renderValues()}
       </div>
-    );
+    )
   }
 }
 
-export default Fib;
+export default Fib
